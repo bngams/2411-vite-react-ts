065f6c4af980d0086eb982d136b1fdbd0a0c4190
bngams <n.boris@devidia.net>
2024-11-21 12:27:14 +0000

    first nav with react router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,27 @@ import Header from './components/layout/header/Header';
 import Footer from './components/layout/footer/Footer';
 import Home from './pages/home/Home';
 import { NextUIProvider } from '@nextui-org/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './pages/about/About';
 
 function App() {
   return (
-    <NextUIProvider>
-      <div className="App">
-        {/* Use a Header custom component */}
-        <Header />
-        <div className="my-content">
-          <Home />
+    <NextUIProvider>              
+      <BrowserRouter> 
+        <div className="App">
+          {/* Use a Header custom component */}
+          <Header />
+          <div className="my-content">
+              <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/product" element={<About />} />
+              </Routes>
+          </div>
+          {/* Use a Footer custom component */}
+          <Footer />
         </div>
-        {/* Use a Footer custom component */}
-        <Footer />
-      </div>
+      </BrowserRouter>
     </NextUIProvider>
   );
 }
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from '@nextui-org/react';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
@@ -10,24 +11,24 @@ const Header: React.FC = () => {
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-4" justify="center">
           <NavbarItem>
-            <Link color="foreground" href="#">
+            <Link color="foreground" to="/home">
               Home
             </Link>
           </NavbarItem>
           <NavbarItem isActive>
-            <Link href="#" aria-current="page">
+            <Link to="/about" aria-current="page">
               About
             </Link>
           </NavbarItem>
           <NavbarItem>
-            <Link color="foreground" href="#">
+            <Link color="foreground" to="/products">
               Products
             </Link>
           </NavbarItem>
         </NavbarContent>
         <NavbarContent justify="end">
           <NavbarItem className="hidden lg:flex">
-            <Link href="#">Login</Link>
+            <Link to="#">Login</Link>
           </NavbarItem>
           <NavbarItem>
             <Button as={Link} color="primary" href="#" variant="flat">
