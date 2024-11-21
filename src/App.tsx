b55cf3a45d73fc1685b1da4986349024ca2c7b81
bngams <n.boris@devidia.net>
2024-11-21 10:44:49 +0000

    :zap: forgot NextUIProvider in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import './App.css';
 import Header from './components/layout/header/Header';
 import Footer from './components/layout/footer/Footer';
 import Home from './pages/home/Home';
+import { NextUIProvider } from '@nextui-org/react';
 
 function App() {
   return (
-    <div className="App">
-      {/* Use a Header custom component */}
-      <Header />
-      <div className="my-content">
-        <Home />
+    <NextUIProvider>
+      <div className="App">
+        {/* Use a Header custom component */}
+        <Header />
+        <div className="my-content">
+          <Home />
+        </div>
+        {/* Use a Footer custom component */}
+        <Footer />
       </div>
-      {/* Use a Footer custom component */}
-      <Footer />
-    </div>
+    </NextUIProvider>
   );
 }
 
